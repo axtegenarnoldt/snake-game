4b43c369deb23d380938590593130d238edc02cc
Josefine axtegen arnoldt <axtegenarnoldt@gmail.com>
2024-04-01 11:21:35 +0000

    Add playgroud window

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import random
+import time
 import gspread
+import curses
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -11,3 +14,17 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("snake_highscore")
+
+
+ # taken from patric lobers youtube
+# playground
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
+
+while True:
+    event = win.getch()
