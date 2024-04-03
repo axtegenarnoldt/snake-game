8104b7e801a5ddf716041c1d7aa82981ba873b30
Josefine axtegen arnoldt <axtegenarnoldt@gmail.com>
2024-04-03 10:53:47 +0000

    add requriements for deployment

--- run.py
+++ run.py
@@ -16,15 +16,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("snake_highscore")
 
-
-
-
 # structur for the playground and how to create snake and food
 # are taken from Patrick Loebers youtube: https://www.youtube.com/watch?v=M_npdRYD4K0
 
 # playground
 curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
+win = curses.newwin(20, 60, 0, 0,)
 win.keypad(1)
 curses.noecho()
 curses.curs_set(0) # hide cursor
@@ -35,13 +32,15 @@ snake = [(4, 4), (4, 3), (4, 2)]
 food = (6, 6)
 
 win.addch(food[0], food[1], '#')
-win.addch(snake[0][0], snake[0][1], '*')
+win.addch(snake[0][0], snake[0][1], '=')
 
 while True:
     event = win.getch()
-    if event == ('q'):
+    if event == ('s'):
+     event = win.getch()
+     if event == ('q'):
         break
-          elif event == curses.KEY_UP:
+    elif event == curses.KEY_UP:
         direction = 'UP'
     elif event == curses.KEY_DOWN:
         direction = 'DOWN'
@@ -49,5 +48,7 @@ while True:
         direction = 'LEFT'
     elif event == curses.KEY_RIGHT:
         direction = 'RIGHT'
+
+
 
       
