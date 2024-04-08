e8635ab3204982af62565ea72b353f4857174dd9
Josefine axtegen arnoldt <axtegenarnoldt@gmail.com>
2024-04-08 08:43:22 +0000

    Add new code for curses playground and changes the code for directions to move the snake

--- run.py
+++ run.py
@@ -18,9 +18,12 @@ SHEET = GSPREAD_CLIENT.open("snake_highscore")
 #structur for the playground and how to create snake and food
 #are taken from Patrick Loebers youtube: https://www.youtube.com/watch?v=M_npdRYD4K0
 
+WINDOW_WIDTH = 60  # number of columns of window box 
+WINDOW_HEIGHT = 20 # number of rows of window box 
+
 # playground
 curses.initscr()
-win = curses.newwin(25, 60, 0, 0)
+win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0)
 win.keypad(1)
 curses.noecho()
 curses.curs_set(0)
@@ -34,18 +37,18 @@ snake = [(5, 4), (4, 3), (4, 2)]
 food = (6, 6)
 
 win.addch(food[0], food[1], '#')
-win.addch(snake[0][0], snake[0][1], '=')
+
 
 # Inital direction of the snake
 direction = 'RIGHT'
 
     
 while True:
-     win.addstr(0, 2, 'score ' + str(score) + '')
-     win.refresh()
-     time.sleep(0.1)
+    win.addstr(0, 2, 'Score ' + str(Score) + '')
+    win.refresh()
+    time.sleep(0.1)
 
-   #Handel user input
+    #Handel user input
     event = win.getch()
     if event == ord('q'):
         break
@@ -59,14 +62,22 @@ while True:
         direction = 'RIGHT'
 
     # Directions to move the snake
+    y = snake[0][0]
+    x = snake[0][1] 
+    if direction == 'DOWN':
+        y += 1
     if direction == 'UP':
-        snake.insert(0, (snake[0][0] -1, snake[0][1]))
-    elif direction == 'DOWN':
-        snake.insert(0, (snake[0][0] +1, snake[0][1]))
-    elif direction == 'LEFT':
-        snake.insert(0, (snake[0][0], snake[0][1] -1))
-    elif direction == 'RIGHT':
-        snake.insert(0, (snake[0][0], snake[0][1] +1))
+        y -= 1
+    if direction == 'LEFT':
+        x -= 1
+    if direction == 'RIGHT':
+        x += 1
+
+
+    snake.insert(0, (y, x)) 
+
+
+    win.addch(snake[0][0], snake[0][1], '=')
 
 # End curses
 curses.endwin()
